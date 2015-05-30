904403a2b52e68ff31305b809695eea46fa0c46d
Michał Glajc <j00s3ph@gmail.com>
2015-05-31 00:48:28 +0200

    zapisywanie połączonych css

--- src/Poznet/CssAsset/CssAsset.php
+++ src/Poznet/CssAsset/CssAsset.php
@@ -14,16 +14,29 @@ namespace Poznet\CssAsset;
 class CssAsset {
     private $maintpl='tpl/main.tpl';
     private $css=array();
-    private $output='css/outpt/';
+    private $output='css/outpt/all.css';
     
     public function __construct() {
         $plik=file_get_contents($this->maintpl);
-        $this->findcss($plik);
+        $css=$this->findcss($plik);
+        $this->saveOutput($css);
     }
     
     public function findcss($txt){
         $pattern="/<link.+href=\"([^\"]+)[^>]+>/";
          preg_match_all ( $pattern , $txt ,  $this->css,PREG_SET_ORDER);
+    }
+    public function combine(){
+        $txt='';
+        $ile=count($this->css);
+        for($i=0;$i<$ile;$i++){
+            $txt.=file_get_contents($this->css[$i]);
+        }
+        return $txt;
+    }
+    
+    public function saveOutput($txt){
+        file_put_contents($this->output, $txt );
     }
     
     
